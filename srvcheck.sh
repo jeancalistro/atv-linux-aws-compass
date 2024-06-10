c12fc4797466a441a36cc01075d33a244fbfbebd
Jean Calistro <jeeanrafael456@gmail.com>
2024-06-10 16:14:06 -0300

    add custom message options

--- srvcheck.sh
+++ srvcheck.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 output_path="./"
 timezone="America/Sao_Paulo"
+active_message="Serviço está ativo"
+inactive_message="Serviço está inativo"
 
-while getopts ":s:o:t:" options; do
+while getopts ":s:o:t:a:i:" options; do
   case ${options} in
     s)
       service=${OPTARG};;
@@ -9,6 +13,10 @@ while getopts ":s:o:t:" options; do
       output_path=${OPTARG};;
     t)
       timezone=${OPTARG};;
+    a)
+      active_message=${OPTARG};;
+    i)
+      inactive_message=${OPTARG};;
     :)
       echo "Opção -${OPTARG} requer um argumento"
       exit 1;;
@@ -18,12 +26,12 @@ while getopts ":s:o:t:" options; do
   esac
 done
 
-isActive=$(systemctl is-active $service)
+status=$(systemctl is-active $service)
 date=$(TZ=${timezone} date +'%d/%m/%Y %H:%M')
 
-if [ "${isActive}" == "active" ]; then
-  echo "${date} + ${service} + ${isActive} + Serviço está ONLINE" >> "${output_path}"/ONLINE.log
+if [ "${status}" == "active" ]; then
+  echo "${date} + ${service} + ${status} + ${active_message}" >> "${output_path}"/ONLINE.log
 
 else
-  echo "${date} + ${service} + ${isActive} + Serviço está OFFLINE" >> "${output_path}"/OFFLINE.log
+  echo "${date} + ${service} + ${status} + ${inactive_message}" >> "${output_path}"/OFFLINE.log
 fi
